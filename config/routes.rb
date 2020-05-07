bacf1193ea48b55c8dd9f36e72bc6aa22cbebcc0
Halahaddad1 <h.m.haddad@gmail.com>
2020-05-07 15:31:13 -0700

    created Pages controller for home page+ drop down menu for drivers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "pages#index.html.erb"
+
+  resources :pages, only: [:index]
+
+  resources :applications
   resources :trips
   resources :passengers
   resources :drivers
