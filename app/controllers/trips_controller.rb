3a4320ab4567bdc90e758757031872ba115351f2
Jessica Stone <jstone4815@gmail.com>
2020-05-11 09:38:46 -0700

    add Show action for Trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,10 @@
 class TripsController < ApplicationController
+  def show
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+    if @trip.nil?
+        head :not_found
+        return
+    end
+end
 end
