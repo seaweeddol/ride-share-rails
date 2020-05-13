abe8ed4863f81d481479defc693c32afa39699c4
Jessica Stone <jstone4815@gmail.com>
2020-05-13 08:15:41 -0700

    add model tests for average rating and total earnings

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -60,20 +60,39 @@ describe Driver do
   # Tests for methods you create should go here
   describe "custom methods" do
     describe "average rating" do
-      it "must return the average rating of all trips/drivers"
+      it "must return the average rating of all trips/drivers" do
+        new_driver.save
+        new_passenger = Passenger.create(name: "Kari", phone_number: "111-111-1211")
+        trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
+        trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 4, cost: 6334)
+        trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
 
+        expect(new_driver.average_rating).must_equal 4
+      end
     end
 
     describe "total earnings" do
-      # Your code here
+      it "must return correct total earnings for a driver" do
+        new_driver.save
+        new_passenger = Passenger.create(name: "Kari", phone_number: "111-111-1211")
+        trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 12)
+        trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 4, cost: 15)
+        trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 246)
+  
+        expect(new_driver.total_revenue).must_equal 214.44
+      end
     end
 
-    describe "can go online" do
-      # Your code here
-    end
-
-    describe "can go offline" do
-      # Your code here
+    describe "can toggle status" do
+      it "will " do
+        new_driver.save
+        new_passenger = Passenger.create(name: "Kari", phone_number: "111-111-1211")
+        trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 12)
+        trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 4, cost: 15)
+        trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 246)
+  
+        expect(new_driver.total_revenue).must_equal 214.44
+      end
     end
 
     # You may have additional methods to test
